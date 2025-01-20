fb2caf86df09c63bbee665535fb209c1397a6d86
amankishore09 <amankishore000@gmail.com>
2025-01-20 14:31:11 +0530

    add main

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-# Define a route for the API
-@app.route('/api/greet', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'World')
-    return jsonify({'message': f'Hello, {name}!'})
+@app.route('/get-rate')
+def get_rate():
+    url = 'https://www.x-rates.com/calculator/?from=USD&to=INR&amount=1'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    main_result = soup.find('span', class_='ccOutputRslt')
+    integer_part = main_result.contents[0].strip()
+    return jsonify({'rate': float(integer_part.replace(',', ''))})
 
 if __name__ == '__main__':
     app.run(debug=True)
